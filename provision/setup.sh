aec573f34885a5f19c5d0e2764a9995140c10a05
Kevin Wojkovich <linuxfanatic91@gmail.com>
2016-04-09 17:50:57 -0400

    Refactor script to setup DB, move apache config file, print git instructions.

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
-echo "Applying the HUBzero secret sauce..."
 
-echo "Developers use example.com, but feel free to change it."
-echo "example.com" > /etc/hostname
+mysql -u root --password="root" --execute "CREATE database example;"
+mysql -u root --password="root" example < /var/www/databasedump.sql
+rm -rf /var/www/public && mkdir /var/www/public
+cp /var/www/default-ssl.conf /etc/apache2/sites-enabled/.
+a2enmod ssl
+service apache2 restart
 
-echo "Configuring packages..."
-echo "deb http://packages.hubzero.org/deb ellie-deb7 main" >> /etc/apt/sources.list
-echo "deb http://download.openvz.org/debian wheezy main" >> /etc/apt/sources.list
 
-apt-key adv --keyserver pgp.mit.edu --recv-keys 143C99EF
-wget http://ftp.openvz.org/debian/archive.key -q -0- | apt-key add -
-apt-get update
+echo "*******************************************************"
+echo " On the HOST:"
+echo " Go to the public directory: "
+echo " Run: git clone http://github.com/hubzero/hubzero-cms"
+echo " Then run: git checkout 2.0.0"
+echo " Then cp -r /var/www/app-scotch /var/www/public/app"
+echo "*******************************************************"
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y hubzero-mysql
-
-apt-get install -y hubzero-mysql
-
-apt-get install -y exim
-
-apt-get install -y hubzero-cms-2.0.0
-
-hzcms install example
-
-hzcms update
-
-a2dissite default default-ssl
-a2endsite example example-ssl
-/etc/init.d/apache2 restart
-
-apt-get install -y hubzero-openldap
-
-hzldap init
-hzcms configure ldap --enable
-hzldap syncuser
